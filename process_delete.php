a802a3412847bd68c640e975c10ba44bc08f0f3b
Fariha07201 <74846991+Fariha07201@users.noreply.github.com>
2021-01-04 11:22:36 +0700

    Add files via upload

---
+++ process_delete.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'db.php';
+
+$alamat =$_GET['alamat'];
+mysql_query("DELETE FROM mahasiswa WHERE alamat='$alamat' ")or die (mysql_error());
+
+header("location:index.php?pesan=hapus");
+?>
